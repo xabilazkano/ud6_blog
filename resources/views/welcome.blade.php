3f1a26b35722ed57baac90819db964ac3a4b8e64
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-12-11 08:21:47 +0100

    Mostrar posts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,56 @@
 @extends('layouts.app')
 @section('content')
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">Vuestro nombre</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
 <!-- Page Content -->
 <div class="container">
   <div class="row">
-    <!-- Post Content Column -->
-    <div class="col-lg-8">
-      <!-- Title -->
-      <h1 class="mt-4">Post title</h1>
-      <!-- Author -->
-      <p class="lead">
-        by
-        <a href="#">Post user</a>
-      </p>
-      <hr>
-      <!-- Date/Time -->
-      <p>Posted on January 1, 2019 at 12:00 PM</p>
-      <hr>
-      <!-- Preview Image -->
-      <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
-      <hr>
-      <!-- Post Content -->
-      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-      <p>normal ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-      <blockquote class="blockquote">
-        <p class="mb-0">blockquote ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-        <footer class="blockquote-footer">Someone famous in
-          <cite title="Source Title">Source Title</cite>
-        </footer>
-      </blockquote>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
-      <hr>
-    </div>
-    <!-- Sidebar Widgets Column -->
-    <div class="col-md-4">
-      
-      <!-- Categories Widget -->
-      <div class="card my-4">
-        <h5 class="card-header">Categories</h5>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-lg-6">
-              <ul class="list-unstyled mb-0">
-                <li>
-                  <a href="#">Web Design</a>
-                </li>
-                <li>
-                  <a href="#">HTML</a>
-                </li>
-                <li>
-                  <a href="#">Freebies</a>
-                </li>
-              </ul>
-            </div>
-            <div class="col-lg-6">
-              <ul class="list-unstyled mb-0">
-                <li>
-                  <a href="#">JavaScript</a>
-                </li>
-                <li>
-                  <a href="#">CSS</a>
-                </li>
-                <li>
-                  <a href="#">Tutorials</a>
-                </li>
-              </ul>
-            </div>
+   @foreach($posts as $post)
+   <!-- Post Content Column -->
+   <div class="col-lg-8">
+    <!-- Title -->
+    <h1 class="mt-4">{{$post->title}}</h1>
+    <!-- Author -->
+    <p class="lead">
+      by
+      <a href="#">{{$post->user}}</a>
+    </p>
+    <hr>
+    <!-- Date/Time -->
+    <p>Posted on January {{$post->published_at}}</p>
+    <hr>
+    @isset($post->image)
+    <!-- Preview Image -->
+    <img class="img-fluid rounded" src="{{asset('$post->image')}}" alt="">
+    @endisset
+    <hr>
+    <!-- Post Content -->
+    <p class="lead">{{$post->body}}</p>
+    <hr>
+  </div>
+  @endforeach
+  <!-- Sidebar Widgets Column -->
+  <div class="col-md-4">
+
+    <!-- Categories Widget -->
+    <div class="card my-4">
+      <h5 class="card-header">Categories</h5>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-lg-6">
+            <ul class="list-unstyled mb-0">
+              @foreach ($categories as $category)
+              <li>
+                <a href="#">{{$category->name}}</a>
+              </li>
+              @endforeach
+            </ul>
           </div>
         </div>
       </div>
-
     </div>
+
   </div>
-  <!-- /.row -->
+</div>
+<!-- /.row -->
 </div>
 <!-- /.container -->
 
