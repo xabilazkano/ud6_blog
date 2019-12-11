e0f450bd9d3cb22bc3075d7110cd0dd79289d6b8
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-12-11 09:26:04 +0100

    Verificar email

--- app/User.php
+++ app/User.php
@@ -6,9 +6,14 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
+
+    public function post()
+    {
+        return $this->hasMany('App\Post');
+    }
 
     /**
      * The attributes that are mass assignable.
